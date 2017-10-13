cae99e1ad6cd3fd8cf93c8e84134f040690bf1ee
Tomasz Burzynski <tomasz.burzynski@getresponse.com>
2017-10-13 16:47:02 +0200

    working console log on react router

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
-import Promise from 'redux-promise';
+import promise from 'redux-promise';
 
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
 
-const createStoreWithMiddleware = applyMiddleware(Promise)(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 
 ReactDOM.render(
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import PostsReducer from './reducer_post';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  posts: PostsReducer
 });
 
 export default rootReducer;
